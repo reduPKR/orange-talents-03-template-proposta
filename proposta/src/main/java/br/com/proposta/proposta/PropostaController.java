93f71b8436f5d40d101b0cc03b2852c1b2b4e3cb
redu <reduardo19942@gmail.com>
2021-04-11 13:13:25 -0300

    Alguns ajustes

--- proposta/src/main/java/br/com/proposta/proposta/PropostaController.java
+++ proposta/src/main/java/br/com/proposta/proposta/PropostaController.java
@@ -5,15 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -23,6 +22,7 @@ public class PropostaController {
     private PropostaRepository propostaRepository;
 
     @PostMapping
+    @Transactional
     public ResponseEntity<?> cadastrar(@RequestBody @Valid PropostaRequest propostaRequest,
                                        UriComponentsBuilder uriComponentsBuilder,
                                        BindingResult result){
@@ -46,5 +46,15 @@ public class PropostaController {
                .collect(Collectors.toList());
 
        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detalhar(@PathVariable long id){
+        Optional<Proposta> proposta = propostaRepository.findById(id);
+
+        if(proposta.isPresent())
+            return ResponseEntity.ok(new PropostaResponse(proposta.get()));
+
+        return ResponseEntity.notFound().build();
     }
 }
