fbb5e098ad21610d5e242174bfe7b1fcfca78fca
redu <reduardo19942@gmail.com>
2021-04-15 16:17:33 -0300

    Cartoes iniciado (indo pra reuniao)

---
+++ proposta/src/main/java/br/com/proposta/cartao/CartaoAssincrono.java
@@ -0,0 +1,21 @@
+package br.com.proposta.cartao;
+
+import br.com.proposta.proposta.AvaliacaoFinanceiraStatus;
+import br.com.proposta.proposta.Proposta;
+import br.com.proposta.proposta.PropostaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CartaoAssincrono {
+    @Autowired
+    private PropostaRepository propostaRepository;
+
+    @Scheduled(fixedDelay = 10000)//cada 10 segundos
+    public void analisarPropostasSemCartoes(){
+        List<Proposta> lista = propostaRepository.findByAvaliacaoFinanceiraStatusAndCardIdIsNull(AvaliacaoFinanceiraStatus.SEM_RESTRICAO);
+    }
+}
--- proposta/src/main/java/br/com/proposta/proposta/PropostaRepository.java
+++ proposta/src/main/java/br/com/proposta/proposta/PropostaRepository.java
@@ -3,7 +3,11 @@ package br.com.proposta.proposta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PropostaRepository extends JpaRepository<Proposta,Long> {
     Boolean existsByDocumento(String documento);
+
+    List<Proposta> findByAvaliacaoFinanceiraStatusAndCardIdIsNull(AvaliacaoFinanceiraStatus status);
 }
