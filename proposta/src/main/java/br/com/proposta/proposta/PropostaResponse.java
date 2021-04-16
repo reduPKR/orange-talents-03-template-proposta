2445cd704b3c1746cbc1c0f2da8e941b629378e3
redu <reduardo19942@gmail.com>
2021-04-16 15:37:14 -0300

    035 finalizado

--- proposta/src/main/java/br/com/proposta/proposta/PropostaResponse.java
+++ proposta/src/main/java/br/com/proposta/proposta/PropostaResponse.java
@@ -14,15 +14,17 @@ public class PropostaResponse {
     private String documento;
     private EnderecoResponse endereco;
     private String status;
+    private String cartao;
 
 
     public PropostaResponse(Proposta proposta) {
+        this.status = proposta.getAvaliacaoFinanceiraStatus();
         this.nome = proposta.getNome();
         this.email = proposta.getEmail();
         this.salario = proposta.getSalario();
         this.documento = proposta.getDocumento();
+        this.cartao = proposta.getCartaoId();
         this.endereco = new EnderecoResponse(proposta.getEndereco());
-        this.status = proposta.getAvaliacaoFinanceiraStatus();
     }
 
     public String getNome() {
@@ -47,5 +49,9 @@ public class PropostaResponse {
 
     public String getStatus() {
         return status;
+    }
+
+    public String getCartao() {
+        return cartao;
     }
 }
