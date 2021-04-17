b7bb2967009f6a3405017a3664d52e79340427dc
redu <reduardo19942@gmail.com>
2021-04-17 16:11:50 -0300

    comecei a fazer a biometria

---
+++ proposta/src/main/java/br/com/proposta/biometria/Biometria.java
@@ -0,0 +1,30 @@
+package br.com.proposta.biometria;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+public class Biometria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotNull
+    private String biometria;
+    @CreationTimestamp
+    private LocalDateTime data;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getBiometria() {
+        return biometria;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+}
